12a67dc8afea4e167f1c0bba80fba13ec6d3d85f
codeHasib <mohammadhasib9827@gmail.com>
2026-03-30 00:10:46 +0600

    add navbar with responsiveness

---
+++ src/components/Nav/Nav.jsx
@@ -0,0 +1,94 @@
+import { ShoppingCart } from "lucide-react";
+import React from "react";
+
+const Nav = () => {
+  return (
+    <>
+      <nav className="border-b border-gray-300">
+        <div className="container mx-auto">
+          <div className="navbar bg-base-100 border-none">
+            <div className="navbar-start">
+              <div className="dropdown">
+                <div
+                  tabIndex={0}
+                  role="button"
+                  className="btn btn-ghost lg:hidden"
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="h-5 w-5"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                  >
+                    {" "}
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="M4 6h16M4 12h8m-8 6h16"
+                    />{" "}
+                  </svg>
+                </div>
+                <ul
+                  tabIndex="-1"
+                  className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
+                >
+                  <li>
+                    <a>Products</a>
+                  </li>
+                  <li>
+                    <a>Features</a>
+                  </li>
+                  <li>
+                    <a>Pricing</a>
+                  </li>
+                  <li>
+                    <a>Testimonials</a>
+                  </li>
+                  <li>
+                    <a>FAQ</a>
+                  </li>
+                </ul>
+              </div>
+              <a className="text-[16px] md:text-xl lg:text-xl font-bold text-purple-500">
+                DigiTools
+              </a>
+            </div>
+            <div className="navbar-center hidden lg:flex">
+              <ul className="menu menu-horizontal px-1">
+                <li>
+                  <a>Products</a>
+                </li>
+                <li>
+                  <a>Features</a>
+                </li>
+                <li>
+                  <a>Pricing</a>
+                </li>
+                <li>
+                  <a>Testimonials</a>
+                </li>
+                <li>
+                  <a>FAQ</a>
+                </li>
+              </ul>
+            </div>
+            <div className="navbar-end">
+              <span>
+                <ShoppingCart></ShoppingCart>
+              </span>
+              <button className="btn btn-ghost p-2 lg:p-4 md:p-4">Login</button>
+              <button className="btn btn-primary rounded-4xl p-2 lg:p-4 md:p-4">
+                {" "}
+                Get Started{" "}
+              </button>
+            </div>
+          </div>
+        </div>
+      </nav>
+    </>
+  );
+};
+
+export default Nav;
